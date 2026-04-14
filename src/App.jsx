0fa2ff2960b3d4862cb6cc7c6992ad35f0b81c99
34621-dev <34621@raphaeldisanto.com.br>
2026-04-14 12:32:21 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <main>
+        <div id="esq"></div>
+
+        <div id="dir">
+          <h1>Activesoft</h1>
+          <p>Fazer login</p>
+          <label htmlFor="">Login</label>
+          <input type="text" />
+          <label htmlFor="">Senha</label>
+          <input type="password" />
+          <button>Entrar</button>
+          <a href="">Esqueceu a senha?</a>
+        </div>
+
+      </main> 
+    </>
+  )
+}
+
+export default App
