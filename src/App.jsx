53ac596c00ce791c9aef722abd61e0101cd0c2ef
34621-dev <34621@raphaeldisanto.com.br>
2026-04-16 12:24:00 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ function App() {
   return (
     <>
       <main>
-        <div id="esq"></div>
+        <div id="esq">
+          <img src="/src" alt="" />
+        </div>
 
         <div id="dir">
           <h1>Activesoft</h1>
           <p>Fazer login</p>
           <label htmlFor="">Login</label>
-          <input type="text" />
+          <input placeholder='Digite seu email...' type="text" />
           <label htmlFor="">Senha</label>
-          <input type="password" />
+          <input placeholder='Digite sua senha...' type="password" />
           <button>Entrar</button>
-          <a href="">Esqueceu a senha?</a>
+          <a href="" className='esqueceu'>Esqueceu a senha?</a>
         </div>
 
       </main> 
